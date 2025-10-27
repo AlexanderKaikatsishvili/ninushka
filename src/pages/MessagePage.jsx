664d106df3ee78905c09b4314e5b89e1c39ea01f
AlexKaikatsishvili <alexkaikatsishvili@yelk.com.ua>
2025-10-27 20:32:52 +0200

    Content

--- src/pages/MessagePage.jsx
+++ src/pages/MessagePage.jsx
@@ -5,9 +5,12 @@ import beauty from './../assets/beauty.jpg';
 import kartina from './../assets/kartina.jpg';
 import nejnost from './../assets/nejnost.jpg';
 import sonya from './../assets/sonya.jpg';
+import playIcon from './../assets/play.svg';
+import lightIcon from './../assets/light.svg';
 
 export const MessagePage = () => {
     const [isPlaying, setIsPlaying] = useState(false);
+    const [isDark, setIsDark] = useState(false);
     const audioRef = useRef(new Audio(soundFile));
     const [count, setCount] = useState(0);
 
@@ -31,48 +34,63 @@ export const MessagePage = () => {
         }
     }
 
+    const makeDark = () => {
+        isDark ? setIsDark(false) : setIsDark(true);
+    }
+
     return (
-        <section className="page">
+        <section className={isDark ? "page dark" : "page"}>
             <div className="page__wrapper">
                 <h1>Ура!!! Це дійсно ти!</h1>
-                <h2>Увімкни для атмосфери)) <button onClick={togglePlay}>{isPlaying ? 'Stop' : 'Play'}</button></h2>
+                <h2>Увімкни для атмосфери)) <button onClick={togglePlay} className="play-btn"><img src={playIcon} alt=""/></button></h2>
                 {
                     isPlaying && <h3>Ну як тобі?)</h3>
                 }
 
-                <p>Я так люблю тебе, маленька! Твою посмішку, твій сміх! Твою щирість та справжність!</p>
+                <h2>Також можеш вимкнути світло для більшої романтики)
+
+                    <button onClick={makeDark} className={isDark ? "switch-btn darken" : "switch-btn"}>
+                        <img src={lightIcon} alt=""/>
+                    </button>
+                </h2>
+
+                <p>Ось і пройшов ще один чудовий, сповнений щастя, місяць! Я так люблю тебе, маленька! Твою посмішку, твій сміх! Твою щирість та справжність!</p>
 
                 <h2>Якщо хочеш побачити круту наєздніцу, то швидко клікай по кнопці багато разів</h2>
                 <div>
                     {count >= 5 &&
-                    <video
-                        width="auto"
-                        height="360"
-                        controls
-                        autoPlay={false}
-                        muted
-                    >
-                        <source src={videoFile} type="video/mp4" />
-                        Ваш браузер не поддерживает видео.
-                    </video>
+                        <video
+                            width="auto"
+                            height="360"
+                            controls
+                            autoPlay={false}
+                            muted
+                        >
+                            <source src={videoFile} type="video/mp4"/>
+                            Ваш браузер не поддерживает видео.
+                        </video>
                     }
                 </div>
 
-                <button onClick={countToShow}>Click me</button>
+                <button className="btn" onClick={countToShow}>Click me</button>
 
 
-                <h2>Ти сповнюєш наш дім такою турботою та щастям!</h2>
-                <img style={{transform: `rotate(${-15*Math.random()}deg)`}} className="photo" src={kartina} alt=""/>
-
-                <h2>Ти тут така ніжна та тендітна! Така жіночна!</h2>
-                <img style={{transform: `rotate(${15*Math.random()}deg)`}} className="photo" src={nejnost} alt=""/>
-
-                <h2>Тут ти моя маленька сонюшка</h2>
-                <img style={{transform: `rotate(${-15*Math.random()}deg)`}} className="photo" src={sonya} alt=""/>
-
-                <h2>Бути поряд з такою жінкою - неймовірне блаженство!</h2>
-                <img style={{transform: `rotate(${15*Math.random()}deg)`}} className="photo" src={beauty} alt=""/>
-
+                <div className="photo-block">
+                    <h2>Ти сповнюєш наш дім такою турботою та щастям!</h2>
+                    <img style={{transform: `rotate(${-15 * Math.random()}deg)`}} className="photo" src={kartina} alt=""/>
+                </div>
+                <div className="photo-block">
+                    <h2>Ти тут така ніжна та тендітна! Така жіночна!</h2>
+                    <img style={{transform: `rotate(${15 * Math.random()}deg)`}} className="photo" src={nejnost} alt=""/>
+                </div>
+                <div className="photo-block">
+                    <h2>Тут ти моя маленька сонюшка</h2>
+                    <img style={{transform: `rotate(${-15 * Math.random()}deg)`}} className="photo" src={sonya} alt=""/>
+                </div>
+                <div className="photo-block">
+                    <h2>Бути поряд з такою жінкою - неймовірне блаженство!</h2>
+                    <img style={{transform: `rotate(${15 * Math.random()}deg)`}} className="photo" src={beauty} alt=""/>
+                </div>
             </div>
         </section>
     )
